cc438d358069011ad733c2ce2c7b78a191e87c0b
jbarrod2510-SMR-A <jbarcelonarodriguez@gmail.com>
2025-01-20 12:27:18 +0100

    Update potencias.js

--- APPWEB/JAVASCRIPT/Actividades/Actividad 10/potencias.js
+++ APPWEB/JAVASCRIPT/Actividades/Actividad 10/potencias.js
@@ -1,15 +1,10 @@
+document.addEventListener("DOMContentLoaded")
 // Función para calcular la potencia
 function calculatePower(base, exponent) {
-    return Math.pow(base, exponent); // Eleva la base al exponente
+    let pow = 1;
+    for(let i = 0; i < exponent; i++){
+        pow = pow * base;
+    }
+    return pow;
 }
-
-// Ejemplo de uso
-const base = parseFloat(prompt("Ingresa la base:"));
-const exponent = parseFloat(prompt("Ingresa el exponente:"));
-
-if (isNaN(base) || isNaN(exponent)) {
-    alert("Por favor, ingresa valores numéricos válidos.");
-} else {
-    const result = calculatePower(base, exponent);
-    alert(`El resultado de ${base} elevado a la potencia ${exponent} es ${result}.`);
-}
+console.log(calculatePower(2,3));
