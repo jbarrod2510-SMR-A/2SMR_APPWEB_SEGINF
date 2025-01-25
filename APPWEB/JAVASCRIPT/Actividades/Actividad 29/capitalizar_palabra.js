c876f88b380268a98494f1b05757786ff3e503d4
Javier <jbarrod2510@g.educaand.es>
2025-01-25 15:36:03 +0100

    mod capitalizar palabara

--- APPWEB/JAVASCRIPT/Actividades/Actividad 29/capitalizar_palabra.js
+++ APPWEB/JAVASCRIPT/Actividades/Actividad 29/capitalizar_palabra.js
@@ -1,24 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    // Función
+    /* Funciones */
     function capitalizar(texto){
+
         let espacio = false;
         let salida = "";
-        for (let i = 0; i<texto.length; i=i+1){
-            if (i==0 || (espacio == true && texto[i] != " ")){
+        
+        for(let i = 0; i < texto.length; i++){
+            if((i == 0) || (espacio == true && texto[i] != " ")){
                 salida += texto[i].toUpperCase();
-                espacio=false;
-            }else if (texto[i] == " "){
-                espacio = true;
-                salida += texto[i];
+                espacio = false;
+            }else if(texto[i] == " "){
+                    espacio = true;
+                    salida += texto[i];
             }else{
-                salida += texto[i]
+                salida += texto[i];
             }
         }
 
         return salida;
+
     }
+
+    /* Llamada a la funcion*/
     console.log(capitalizar("hola mundo")) // "Hola Mundo"
-    console.log(capitalizar("make   it   real")) // "Make It Real"
+    console.log(capitalizar("make    it    real")) // "Make It Real"
     console.log(capitalizar("")) // ""
+
+
+
 });
