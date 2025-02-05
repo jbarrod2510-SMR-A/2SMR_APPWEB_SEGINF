1456dc4d01a917c557d285a7167d264dfe78e304
jbarrod2510 <jbarrod2510@g.educaand.es>
2025-02-05 14:00:45 +0100

    5 de febrero 5.0

--- APPWEB/proyecto/caja/javascript/script.js
+++ APPWEB/proyecto/caja/javascript/script.js
@@ -1,5 +1,15 @@
-document.addEventListener("DOMContentLoaded", function(){
+function toggleBox(){
+    if(caja.style.display == "none"){
+        caja.style.display == "block";
+        boton.textContent = "Ocultar la caja";
+    }else{
+        caja.style.display = "none";
+        boton.textContent = "Mostrar caja";
+    }
+}
 
-    
+const caja = document.querySelector(".cuadrado");
+const boton = document.querySelector(".ocultar");
 
-})
+
+button.addEventListener("click", toggleBox);
