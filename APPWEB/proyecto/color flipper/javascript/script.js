c828c1aa93daaab0f2e0c2157f95c423cc60305b
Javier <jbarrod2510@g.educaand.es>
2025-02-08 15:19:53 +0100

    8 febrero color flipper casa

--- APPWEB/proyecto/color flipper/javascript/script.js
+++ APPWEB/proyecto/color flipper/javascript/script.js
@@ -1,5 +1,14 @@
-function colorSimple(){
+const colors = ["red", "yellow", "green", "purple", "pink", "blue", "brown", "orange", "gray", "black", "aqua",
+    "cyan", "magenta", "gold", "silver", "teal", "lime", "maroon", "navy", "olive"];
 
-    clr = ["red", "yellow", "green", "purple", "pink", "blue", "brown"]
-    
-}
+const button = document.querySelector(".click");
+const backgroundText = document.querySelector(".background");
+const mainSection = document.querySelector("main"); // Seleccionamos el <main> en lugar del body
+
+button.addEventListener("click", function () {
+let randomIndex = Math.floor(Math.random() * colors.length);
+let randomColor = colors[randomIndex];
+
+mainSection.style.backgroundColor = randomColor; // Cambiamos el color SOLO del <main>
+backgroundText.innerHTML = `Background color: <span style="color: ${randomColor}; font-weight: bold;">${randomColor.charAt(0).toUpperCase() + randomColor.slice(1)}</span>`;
+});
