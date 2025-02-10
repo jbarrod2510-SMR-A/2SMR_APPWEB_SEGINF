2d2e5954f32a4c8c9da59c8e95e0603d2cd9f9e3
Javier <jbarrod2510@g.educaand.es>
2025-02-10 15:45:27 +0100

    10 de febrero

--- APPWEB/proyecto/color flipper/javascript/script.js
+++ APPWEB/proyecto/color flipper/javascript/script.js
@@ -1,20 +1,14 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const boton = document.querySelector(".click");
-    const titulo = document.querySelector(".background");
-
-    
-    const colors = ["red", "yellow", "green", "purple", "pink", "blue", "brown", "orange", "gray", "greenyellow", "aqua",
+const colors = ["red", "yellow", "green", "purple", "pink", "blue", "brown", "orange", "gray", "greenyellow", "aqua",
     "cyan", "magenta", "gold", "silver", "teal", "lime", "maroon", "navy", "olive"];
 
-    function coloresRandom(){
-        return colors[Math.floor(Math.random() * colors.length)];
-    }
-
-    boton.addEventListener("click", function(){
-        const colorNuevo = coloresRandom();
-        document.body.style.backgroundColor= colorNuevo;
-        titulo.textContent = `Background Color: ${colorNuevo}`;
-        titulo.style.color = colorNuevo;
-        boton.style.backgroundColor = colorNuevo
-    })
-})
+const button = document.querySelector(".click");
+const background = document.querySelector(".background");
+const body = document.body;
+    
+button.addEventListener("click", function () {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    const randomColor = colors[randomIndex];
+        
+    body.style.backgroundColor = randomColor;
+    background.textContent = `Background Color: ${randomColor}`;
+    });
