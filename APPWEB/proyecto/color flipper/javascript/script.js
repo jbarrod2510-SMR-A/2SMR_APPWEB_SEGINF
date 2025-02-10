03bba9f856396a18952c705088352a4d9207cf9d
jbarrod2510 <jbarrod2510@g.educaand.es>
2025-02-10 08:42:59 +0100

    10 de febrero 2.0

--- APPWEB/proyecto/color flipper/javascript/script.js
+++ APPWEB/proyecto/color flipper/javascript/script.js
@@ -1,14 +1,20 @@
-const colors = ["red", "yellow", "green", "purple", "pink", "blue", "brown", "orange", "gray", "greenyellow", "aqua",
+document.addEventListener("DOMContentLoaded", function(){
+    const boton = document.querySelector(".click");
+    const titulo = document.querySelector(".background");
+
+    
+    const colors = ["red", "yellow", "green", "purple", "pink", "blue", "brown", "orange", "gray", "greenyellow", "aqua",
     "cyan", "magenta", "gold", "silver", "teal", "lime", "maroon", "navy", "olive"];
 
-const button = document.querySelector(".click");
-const backgroundText = document.querySelector(".background");
-const mainSection = document.querySelector("main"); // Seleccionamos el <main> en lugar del body
+    function coloresRandom(){
+        return colors[Math.floor(Math.random() * colors.length)];
+    }
 
-button.addEventListener("click", function () {
-let randomIndex = Math.floor(Math.random() * colors.length);
-let randomColor = colors[randomIndex];
-
-mainSection.style.backgroundColor = randomColor; // Cambiamos el color solo del <main>
-backgroundText.innerHTML = `Background color: <span style="color: ${randomColor}; font-weight: bold;">${randomColor.charAt(0).toUpperCase() + randomColor.slice(1)}</span>`;
-});
+    boton.addEventListener("click", function(){
+        const colorNuevo = coloresRandom();
+        document.body.style.backgroundColor= colorNuevo;
+        titulo.textContent = `Background Color: ${colorNuevo}`;
+        titulo.style.color = colorNuevo;
+        boton.style.backgroundColor = colorNuevo
+    })
+})
