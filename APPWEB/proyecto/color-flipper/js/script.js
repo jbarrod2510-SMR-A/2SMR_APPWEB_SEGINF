27022b83f4a07be4643c97aecbb49f25edfc5b13
Javier <jbarrod2510@g.educaand.es>
2025-02-13 16:49:19 +0100

    13 febrero casa

--- APPWEB/proyecto/color-flipper/js/script.js
+++ APPWEB/proyecto/color-flipper/js/script.js
@@ -1,17 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const button = document.querySelector(".boton");
     const title = document.querySelector(".titulo");
-
     const colores = [
         "Red", "Blue", "Green", "Yellow", "Orange", "Purple", "Pink", "Brown", 
         "Cyan", "Magenta", "Lime", "Yellowgreen", "Aqua", "Olive", "Teal", "Turquoise", 
         "Violet", "Gold", "Silver", "Coral"
     ];
-
     function getRandomColor() {
         return colores[Math.floor(Math.random() * colores.length)];
     }
-
     button.addEventListener("click", function () {
         const newColor = getRandomColor();
         document.body.style.backgroundColor = newColor;
